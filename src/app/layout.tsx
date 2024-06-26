671794697e5fca5aa34445b31fd8647db569526f
reza18s <swary2021@gmail.com>
2024-06-26 17:02:54 +0330

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import { ClerkProvider } from '@clerk/nextjs';
-import { dark } from '@clerk/themes';
 import { ThemeProvider } from '@/providers/theme-provider';
 import ModalProvider from '@/providers/modal-provider';
 import { Toaster } from '@/components/ui/toaster';
